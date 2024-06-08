dbf2f271e8cf8a08bf1cc9ce02b4d33b481faad2
Nazmus Sakib <sakib.braincraft@gmail.com>
2024-06-08 20:39:12 +0600

    added bottom navigation

--- app/src/main/java/com/example/kotlingptjetpack/MainActivity.kt
+++ app/src/main/java/com/example/kotlingptjetpack/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlingptjetpack
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,6 +10,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Response
@@ -21,6 +23,7 @@ import retrofit2.http.GET
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,6 +32,24 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, NewsFragment())
                 .commit()
+        }
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            var selectedFragment: Fragment? = null
+            when (item.itemId) {
+                R.id.nav_home -> selectedFragment = NewsFragment() // Replace with appropriate fragment
+                R.id.nav_search -> selectedFragment =NewsFragment() // Replace with appropriate fragment
+                R.id.nav_profile -> selectedFragment =NewsFragment() // Replace with appropriate fragment
+                R.id.nav_settings -> selectedFragment = NewsFragment() // Replace with appropriate fragment
+                R.id.nav_more -> selectedFragment = NewsFragment() // Replace with appropriate fragment
+            }
+            if (selectedFragment != null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit()
+            }
+            true
         }
     }
 }
