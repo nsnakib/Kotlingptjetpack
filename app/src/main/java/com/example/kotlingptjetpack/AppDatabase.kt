b8516865ba3ece4197bde2557684624304340bcc
Nazmus Sakib <sakib.braincraft@gmail.com>
2024-07-17 14:16:51 +0600

    db migration

--- app/src/main/java/com/example/kotlingptjetpack/AppDatabase.kt
+++ app/src/main/java/com/example/kotlingptjetpack/AppDatabase.kt
@@ -18,7 +18,9 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "article_database"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
                 INSTANCE = instance
                 instance
             }
